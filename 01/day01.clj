d9ee64dedb81114fa2c0f6b7184807da41a57bfc
Dreary Lisper <drearylisper@gmail.com>
2023-12-01 15:26:35 +0000

    More details to Day 01.

--- 01/day01.clj
+++ 01/day01.clj
@@ -10,14 +10,34 @@
 
 ;; # Day 01: Trebuchet?! 
 
-;; ## Part 1
+;; ## Part 01
+
+;; You try to ask why they can't just use a [weather machine](https://adventofcode.com/2015/day/1) ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a [trebuchet](https://en.wikipedia.org/wiki/Trebuchet) ("please hold still, we need to strap you in").
+
+;; As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been *amended** by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+
+;; The newly-improved calibration document consists of lines of text; each line originally contained a specific **calibration value** that the Elves now need to recover. On each line, the calibration value can be found by combining the **first digit** and the **last digit** (in that order) to form a single **two-digit number.**
+
+;; For example:
+
+;; ```
+;; 1abc2
+;; pqr3stu8vwx
+;; a1b2c3d4e5f
+;; treb7uchet
+;; ```
+
+;; In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces **142**.
+
+;; Consider your entire calibration document. **What is the sum of all of the calibration values?**
+
 
 ;; Helper function for using solutions on different type of inputs
 (defn solve-with [filename solution]
   (with-open [rdr (io/reader filename)]
     (solution (line-seq rdr))))
 
-;; First function we need is which will be extracting digits from the line
+;; The first function that we need will be extracting digits from the input line
 (defn parse-line-part01 [line]
   (->> line
        (filter #(Character/isDigit %))
@@ -43,6 +63,26 @@
 
 ;; Answer for the first part!
 (solve-with "01/input.txt" part01)
+
+;; ## Part 02
+
+;; Your calculation isn't quite right. It looks like some of the digits are actually **spelled out with letters**: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+;; Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+;; ```
+;; two1nine
+;; eightwothree
+;; abcone2threexyz
+;; xtwone3four
+;; 4nineeightseven2
+;; zoneight234
+;; 7pqrstsixteen
+;; ```
+
+;; In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces **281**.
+
+;; **What is the sum of all of the calibration values?**
 
 ;; To solve second part we need extracting overlapping 
 ;; string that may define numbers as well.
--- index.clj
+++ index.clj
@@ -1,6 +1,31 @@
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(ns index
+  (:require [nextjournal.clerk :as clerk]))
+
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(comment
+  (clerk/serve! {:paths [".", "01"] :watch-paths ["."]}))
+
 ;; # 🎄 Advent of Code 2023 in Clojure
 ;;
-;; [Advent of Code](https://adventofcode.com/2023)
 ;;
-(ns index
-  {:nextjournal.clerk/visibility {:code :hide :result :hide}})
+
+^{:nextjournal.clerk/visibility {:code :hide :result :show}}
+(clerk/html "<img src='https://raw.githubusercontent.com/DrearyLisper/aoc-2023/master/images/logo.png' alt='logo' style='width:600px; text-align: center;'/>")
+
+;; You can find solution with detailed explanations below.
+;;
+;; Please enjoy!
+;;
+;; Solutions are authored by [@DrearyLisper](https://x.com/drearylisper)
+;;
+;; Don't forget to star my [Project on GitHub](https://github.com/DrearyLisper/aoc-2023)!
+
+;; ## [Day 01: Trebuchet?!](01/day01)
+;; ``` clojure
+;; aoc-2023$ clj -X day01/run
+;; 55017
+;; 53539
+;; ```
+
+
